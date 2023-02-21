9e8293f40a3db13591719479e5df292093c3a140
João Bispo <joaobispo2077@gmail.com>
2023-02-21 11:28:35 -0300

    docs: add buffer definition

---
+++ docs/examples/buffers/fundamentals.js
@@ -0,0 +1,4 @@
+// Buffers => Represetação de um espaço na memória do computador utilizado para transitar dados de uma maneira muito rápida.
+
+const buffer = Buffer.from("ok");
+console.log(buffer);
