b6c202723ba6a3a032b447c8808bfd240093b8c9
João Bispo <joaobispo2077@gmail.com>
2023-02-21 09:54:03 -0300

    feat: add GET and POST handlers

--- src/server.js
+++ src/server.js
@@ -2,8 +2,27 @@ import http from 'node:http';
 
 const PORT = 3333;
 
+const users = [];
+
 const server = http.createServer((request, response) => {
-  return response.end("Hello World!");
+  const { method, url } = request;
+
+  if (method === 'GET' && url === '/users') {
+    response.setHeader('Content-type', 'application/json');
+    return response.end(JSON.stringify(users));
+  }
+
+  if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@xample.com'
+    });
+
+    return response.writeHead(201).end();
+  }
+
+  return response.writeHead(404).end();
 });
 
 server.listen(PORT);
