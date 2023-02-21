a9c83a837b72f0fef2639db8605c2a517dc72e2a
João Bispo <joaobispo2077@gmail.com>
2023-02-21 11:28:54 -0300

    feat: extract json conversion to a middleware

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import http from 'node:http';
+import { json } from './middlewares/json.js';
 
 const PORT = 3333;
 
@@ -7,17 +8,7 @@ const users = [];
 const server = http.createServer(async (request, response) => {
   const { method, url } = request;
 
-  const buffers = [];
-
-  for await (const chunk of request) {
-    buffers.push(chunk);
-  }
-
-  try {
-    request.body = JSON.parse(Buffer.concat(buffers).toString());
-  } catch (err) {
-    request.body = null;
-  }
+  await json(request, response);
 
   if (method === 'GET' && url === '/users') {
     response.setHeader('Content-type', 'application/json');
