d244e3b815ec4aa3a1d20c3b121277eccc86a4b8
João Bispo <joaobispo2077@gmail.com>
2023-02-21 11:14:29 -0300

    feat: handle request json body as chunks

--- src/server.js
+++ src/server.js
@@ -4,8 +4,20 @@ const PORT = 3333;
 
 const users = [];
 
-const server = http.createServer((request, response) => {
+const server = http.createServer(async (request, response) => {
   const { method, url } = request;
+
+  const buffers = [];
+
+  for await (const chunk of request) {
+    buffers.push(chunk);
+  }
+
+  try {
+    request.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch (err) {
+    request.body = null;
+  }
 
   if (method === 'GET' && url === '/users') {
     response.setHeader('Content-type', 'application/json');
@@ -13,10 +25,12 @@ const server = http.createServer((request, response) => {
   }
 
   if (method === 'POST' && url === '/users') {
+    const { name, email } = request.body;
+
     users.push({
-      id: 1,
-      name: 'John Doe',
-      email: 'johndoe@xample.com'
+      id: users.length + 1,
+      name,
+      email
     });
 
     return response.writeHead(201).end();
