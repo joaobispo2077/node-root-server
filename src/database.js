112fe71ecba184445c6064c6c6c3bae6aa483fe7
João Bispo <joaobispo2077@gmail.com>
2023-05-08 18:43:51 -0300

    feat: persist in memory database and a JSON database

--- src/database.js
+++ src/database.js
@@ -1,5 +1,27 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+
 export class Database {
+  #databasePath = new URL('../database.json', import.meta.url);
   #data = {};
+
+  constructor() {
+    this.#load();
+  }
+
+  #load() {
+    fs.readFile(this.#databasePath, 'utf-8')
+      .then(data => {
+        this.#data = JSON.parse(data);
+      })
+      .catch(() => {
+        this.#persist();
+      });
+  }
+
+  #persist() {
+    fs.writeFile(this.#databasePath, JSON.stringify(this.#data, null, 2));
+  }
 
   select(table) {
     const data = this.#data[table] ?? [];
@@ -13,6 +35,8 @@ export class Database {
       this.#data[table] = [data];
 
     }
+
+    this.#persist();
 
     return data;
   }
